9b112428b1f72271f4af4da942dab06e5681ae7d
Vladimir de Turckheim <vlad2t@hotmail.com>
2018-06-01 15:19:20 +0200

    add sample data

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
+const Fs = require('fs');
 const SlackClient = require('@slack/client');
 // SNIP: the initialization code shown above is skipped for brevity
 
 const rtm = new SlackClient.RTMClient(require('./secret').slack_token);
 rtm.start();
 
+let i = 0;
 rtm.on('message', (event) => {
-
-    console.log(event);
 
 });
