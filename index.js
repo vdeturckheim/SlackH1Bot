e3417001d57306f345bf577656931b0cd5bd9015
Vladimir de Turckheim <vlad2t@hotmail.com>
2018-06-01 12:52:11 +0200

    start bot

--- index.js
+++ index.js
@@ -2,18 +2,11 @@
 const SlackClient = require('@slack/client');
 // SNIP: the initialization code shown above is skipped for brevity
 
-const rtm = new SlackClient.RTMClient(process.env.SLACK_TOKEN);
-
+const rtm = new SlackClient.RTMClient(require('./secret').slack_token);
+rtm.start();
 
 rtm.on('message', (event) => {
-    // For structure of `event`, see https://api.slack.com/events/message
 
-    // Skip messages that are from a bot or my own user ID
-    if ( (message.subtype && message.subtype === 'bot_message') ||
-        (!message.subtype && message.user === rtm.activeUserId) ) {
-        return;
-    }
+    console.log(event);
 
-    // Log the message
-    console.log(`(channel:${message.channel}) ${message.user} says: ${message.text}`);
 });
