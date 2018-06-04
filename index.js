bf355e589ce19663cac21dba821b9f4b287d1a45
Vladimir de Turckheim <vlad2t@hotmail.com>
2018-06-04 21:25:36 +0200

    add first H1 reporting

--- index.js
+++ index.js
@@ -8,30 +8,36 @@ const Email = require('./lib/email');
 const rtm = new SlackClient.RTMClient(require('./secret').slack_token);
 rtm.start();
 
+
+const handleEvent = function(event) {
+
+    if (event.username !== 'HackerOne') {
+        return; // let's ignore other messages
+    }
+    const fallback = event.attachments[0].fallback;
+
+    if (fallback.startsWith('New HackerOne report')) {
+        // we are in the case of a new report
+        const reportId = fallback.match(/#(\d+)/)[1];
+        console.log('new report', reportId);
+        return HackerOne.writeComment(reportId, Messages.thanksForReporting(), false);
+    }
+    if (fallback.startsWith('New HackerOne comment')) {
+        // we are in the case of a new comment
+        const text = event.attachments[0].text;
+        if (text.startsWith('/notify_maintainer')) {
+            // this is an instruction to the bot to notify the user whose email has been given and offer them to join
+
+        }
+    }
+};
+
 rtm.on('message', (event) => {
 
-    try {
-        if (event.username !== 'HackerOne') {
-            return; // let's ignore other messages
-        }
-        const fallback = event.attachments[0].fallback;
+    handleEvent(event)
+        .catch((err) => {
 
-        if (fallback.startsWith('New HackerOne report')) {
-            // we are in the case of a new report
-            const reportId = fallback.match(/#(\d+)/);
-            HackerOne.writeComment(reportId, Messages.thanksForReporting(), false);
-        }
-        if (fallback.startsWith('New HackerOne comment')) {
-            // we are in the case of a new comment
-            const text = event.attachments[0].text;
-            if (text.startsWith('/notify_maintainer')) {
-                // this is an instruction to the bot to notify the user whose email has been given and offer them to join
-
-            }
-        }
-    }
-    catch (e) {
-        console.error(e);
-    }
-
+            console.error(event);
+            console.error(err);
+        });
 });
