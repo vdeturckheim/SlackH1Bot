2d1cf04174d5c355672185aa8e1599a6027f420b
Vladimir de Turckheim <vlad2t@hotmail.com>
2018-06-09 17:19:16 +0200

    email is working

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ rtm.start();
 
 const handleError = function (err, event) {
 
+    return Promise.resolve();
 };
 
-const handleEvent = function(event) {
+const handleEvent = function (event) {
 
     if (event.username !== 'HackerOne') {
-        return; // let's ignore other messages
+        return Promise.resolve(); // let's ignore other messages
     }
     const fallback = event.attachments[0].fallback;
 
@@ -24,21 +25,28 @@ const handleEvent = function(event) {
         // we are in the case of a new report
         const reportId = fallback.match(/#(\d+)/)[1];
         console.log('new report', reportId);
-        return HackerOne.writeComment(reportId, Messages.thanksForReporting(), false)
-            .catch((err) => handleError(err, event));
+        return HackerOne.writeComment(reportId, Messages.thanksForReporting(), false);
     }
     if (fallback.startsWith('New HackerOne comment')) {
         // we are in the case of a new comment
         const text = event.attachments[0].text;
         if (text.startsWith('/notify_maintainer')) {
             // this is an instruction to the bot to notify the user whose email has been given and offer them to join
-            const email = text.split(' ').find((x) => IsEmail(x));
-            if (!email) {
-                return handleError(new Error('could not find email'), event); // TODO: err message
+            let email;
+            try {
+                email = text.split(' ')
+                    .find((x) => x.includes('mailto'))
+                    .split('|')[1].split('>')[0];
+            }
+            catch (e) {
+                return Promise.reject(new Error('could not find email'), event); // TODO: err message
+            }
+
+            if (!email || !IsEmail.validate(email)) {
+                return Promise.reject(new Error('could not find email'), event); // TODO: err message
             }
             const msg = Messages.NotifyMaintainer();
-            return Email.send(email, msg.topic, msg.value)
-                .catch((err) => handleError(err, event));
+            return Email.send(email, msg.topic, msg.value);
         }
     }
 };
@@ -46,6 +54,7 @@ const handleEvent = function(event) {
 rtm.on('message', (event) => {
 
     handleEvent(event)
+        .then((...args) => console.log(args))
         .catch((err) => {
 
             console.error(event);
